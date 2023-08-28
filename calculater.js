6b9d669baaa0bae1ffa93113b52c4f0cc0341f2b
RehabAlkaini <143426611+RehabAlkaini@users.noreply.github.com>
2023-08-28 20:11:14 +0300

    Add files via upload

---
+++ calculater.js
@@ -0,0 +1,26 @@
+let output = document.getElementById("calc")
+
+function Display(num){
+output.value += num
+
+
+}
+function Clear(){
+output.value ="";
+
+}
+function Delete(){
+    output.value = output.value.slice(0,-1);
+
+}
+
+function calac(){
+    try{
+        output.value = eval(output.value);
+
+    }
+    catch(err){
+
+output.value="Error";
+    }
+}
